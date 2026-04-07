f6007af875e198071757cd9fb49e5585f88b0a4c
Santiago Riaño Cordoba <santiagorianothree@gmail.com>
2026-04-07 04:14:47 -0500

    feat: se agregó metodo para aumentar debilidad

--- src/gestionproyectos/Mago.java
+++ src/gestionproyectos/Mago.java
@@ -1,9 +1,17 @@
 float resistencia = 0
 float poder = 0
+int debilidad = 0
+int daño = 0
 
 aumentarPoder(){
 poder++}
 
 aumentarResistencia(){
 resistencia++
+}
+
+AumentarDebilidad(){
+if daño > 5{
+debilidad++
+}
 }
